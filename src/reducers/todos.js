6b8a96e4cfe09eee77aa699813e5789fbf0323e0
hokuma <okuma@hokuma.net>
2018-07-15 01:04:50 +0900

    todo reducer

---
+++ src/reducers/todos.js
@@ -0,0 +1,34 @@
+import { handleActions } from 'redux-actions';
+
+const defaultState = {
+  todos: [],
+  filterLabelId: null
+};
+
+const index = (state, {payload}) => Object.assign({}, state, {todos: payload});
+const add = (state, {payload}) => Object.assign({}, state, {todos: state.todos.concat(payload)});
+const remove = (state, {payload}) => Object.assign({}, state, { todos: state.todos.filter((todo) => todo.id !== payload.id)});
+const update = (state, {payload}) => Object.assign({}, state, { todos: updateTodo(payload, state.todos) });
+const filter = (state, {payload}) => Object.assign({}, state, { filterLabelId: payload.filterLabelId });
+
+const updateTodo = (payload, todos) => {
+  return todos.reduce((acc, todo) => {
+    if(todo.id === payload.id) {
+      todo = payload;
+    }
+    return acc.concat(todo);
+  }, []);
+};
+
+const reducer = handleActions({
+  TODOS: {
+    INDEX: index,
+    ADD: add,
+    REMOVE: remove,
+    FILTER: filter,
+    UPDATE: update,
+    TOGGLE: update
+  },
+}, defaultState);
+
+export default reducer;
