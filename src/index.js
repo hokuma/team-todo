8b22b1533ed17d61201bfd3e41ee2526204b0637
hokuma <okuma@hokuma.net>
2018-07-15 22:07:06 +0900

    create store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import todosReducer from './reducers/todos';
 import 'normalize.css/normalize.css';
 import App from './components/App';
+
+const store = createStore(
+  todosReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
